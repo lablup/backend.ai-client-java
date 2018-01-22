e1ea3309e076191e7f7d5e53256fbe1fde9d2149
Jaepil Koh <gofeel@gmail.com>
2018-01-22 13:30:33 +0900

    Update tester

--- backend.ai-client-tester/src/main/java/ai/backend/clienttester/Main.java
+++ backend.ai-client-tester/src/main/java/ai/backend/clienttester/Main.java
@@ -2,17 +2,21 @@ package ai.backend.clienttester;
 
 import ai.backend.client.ClientConfig;
 import ai.backend.client.Kernel;
-import ai.backend.client.values.ExecutionMode;
-import ai.backend.client.values.ExecutionResult;
 import ai.backend.client.exceptions.AuthorizationFailureException;
 import ai.backend.client.exceptions.ConfigurationException;
 import ai.backend.client.exceptions.NetworkFailureException;
+import ai.backend.client.values.ExecutionMode;
+import ai.backend.client.values.ExecutionResult;
 import ai.backend.client.values.RunStatus;
+import com.google.gson.JsonObject;
 import org.apache.commons.cli.*;
+import org.apache.commons.io.FilenameUtils;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.UnsupportedCharsetException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.SEVERE;
@@ -23,80 +27,107 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Kernel kernel;
+
+        CommandLine cmd;
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd;
         Options options = new Options();
 
-        Option inputFileOpt = new Option("f", "file", true, "Input file path");
-        inputFileOpt.setRequired(true);
-        options.addOption(inputFileOpt);
+        options.addOption(Option.builder("k")
+                .longOpt("kernel")
+                .hasArg()
+                .desc("Kernel type")
+                .required()
+                .build());
+        options.addOption(Option.builder("a")
+                .longOpt("accesskey")
+                .hasArg()
+                .desc("Backend AI accesskey")
+                .required(false)
+                .build());
+        options.addOption(Option.builder("s")
+                .longOpt("secretkey")
+                .hasArg()
+                .desc("Backend AI secretkey")
+                .required(false)
+                .build());
+        options.addOption(Option.builder()
+                .longOpt("encoding")
+                .hasArg(true)
+                .required(false)
+                .desc("Encoding of the input file")
+                .build());
+        options.addOption(Option.builder("b")
+                .longOpt("buildcmd")
+                .hasArg()
+                .desc("Command for build")
+                .required(false)
+                .build());
+        options.addOption(Option.builder("e")
+                .longOpt("execcmd")
+                .hasArg()
+                .desc("Command for build")
+                .required(false)
+                .build());
 
-        Option kernelTypeOpt = new Option("k", "kernel", true, "Kernel type");
-        kernelTypeOpt.setRequired(true);
-        options.addOption(kernelTypeOpt);
-
-        Option accessKeyOpt = new Option("a", "accesskey", true, "Backend AI accesskey");
-        accessKeyOpt.setRequired(false);
-        options.addOption(accessKeyOpt);
-
-        Option secretKeyOpt = new Option("s", "secretkey", true, "Backend AI secretkey");
-        secretKeyOpt.setRequired(false);
-        options.addOption(secretKeyOpt);
-
-        Option encodingOpt = new Option("e", "encoding", true, "Encoding of the input file");
-        encodingOpt.setRequired(false);
-        options.addOption(encodingOpt);
+        options.addOption(Option.builder("d")
+                .longOpt("basedirectory")
+                .hasArg()
+                .desc("Base")
+                .required(false)
+                .build());
 
         try {
             cmd = parser.parse(options, args);
         } catch (org.apache.commons.cli.ParseException e) {
-            System.out.println(e.getMessage());
-            formatter.printHelp("utility-name", options);
-
+            System.err.println(e.getMessage());
+            formatter.printHelp("java -jar backend.ai-client-test.jar", options);
             System.exit(1);
             return;
         }
 
-        Kernel kernel;
-        String code;
+        String[] x = cmd.getArgs();
+        String basedir = cmd.getOptionValue("d", ".");
 
+        String base = "";
         try {
-            code = readFile(cmd.getOptionValue("file"), Charset.forName(cmd.getOptionValue("encoding", "UTF-8")));
+            base = getBaseDirectory(basedir);
         } catch (IOException e) {
-            LOGGER.log(SEVERE,"ERROR: Can't open the file");
-            return;
-        } catch (UnsupportedCharsetException e) {
-            LOGGER.log(SEVERE, "ERROR: Bad encoding");
-            return;
+            System.err.println("Invalid target directory");
+            System.exit(1);
         }
+        HashMap<String, String> target_files = new HashMap<String, String>();
 
+        for(String el : x) {
+            try {
+                String t = getUnixRelativePath(base, el);
+                target_files.put(t, el);
+            } catch (IOException e) {
+                System.err.println("Invalid target file error. Check your file exists under the base directory.");
+                System.exit(1);
+            }
+        }
         try {
             kernel = createKernel(cmd);
         } catch (ConfigurationException e) {
             System.err.println("Bad ClientConfig");
             return;
         }
-        runCode(kernel, code);
+        LOGGER.info(String.format("Kernel is ready : %s", kernel.getId()));
+
+        uploadFiles(kernel, target_files);
+
+        String buildCmd = cmd.getOptionValue("b", "*");
+        String execCmd = cmd.getOptionValue("e", "*");
+        runCode(kernel, buildCmd, execCmd);
+
         finish(kernel);
+
     }
 
-    static String readFile(String path, Charset encoding)
-            throws IOException
-    {
-        FileInputStream stream = new FileInputStream(path);
-        try {
-            Reader reader = new BufferedReader(new InputStreamReader(stream, encoding));
-            StringBuilder builder = new StringBuilder();
-            char[] buffer = new char[8192];
-            int read;
-            while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
-                builder.append(buffer, 0, read);
-            }
-            return builder.toString();
-        } finally {
-            stream.close();
-        }
+    private static void uploadFiles(Kernel kernel, HashMap<String, String> files) {
+        kernel.upload(files);
     }
 
     private static Kernel createKernel(CommandLine cmd) throws ConfigurationException {
@@ -134,13 +165,17 @@ public class Main {
         return kernel;
     }
 
-    public static void runCode(Kernel kernel, String code) {
+    public static void runCode(Kernel kernel, String buildCmd, String execCmd) {
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-        LOGGER.info(String.format("Kernel is ready : %s", kernel.getId()));
-        ExecutionMode mode = ExecutionMode.QUERY;
+        ExecutionMode mode = ExecutionMode.BATCH;
         String runId = Kernel.generateRunId();
+        String code = "";
         while (true) {
-            ExecutionResult result = kernel.execute(mode, runId, code, null);
+            JsonObject opts = new JsonObject();
+            opts.addProperty("build", buildCmd);
+            opts.addProperty("exec", execCmd);
+
+            ExecutionResult result = kernel.execute(mode, runId, code, opts);
             System.out.print(result.getStdout());
             System.err.print(result.getStderr());
             if (result.isFinished()) {
@@ -162,5 +197,31 @@ public class Main {
 
     private static void finish(Kernel kernel) {
         kernel.destroy();
+    }
+
+    protected static String getBaseDirectory(String base) throws IOException {
+        File fp = new File(base);
+        if (fp.exists() && fp.isDirectory()) {
+            String normalizedPath = FilenameUtils.normalize(new File(base).getAbsolutePath());
+            if(!normalizedPath.endsWith(File.separator)) {
+                normalizedPath = normalizedPath + File.separator;
+            }
+            return normalizedPath;
+        }
+        throw new IOException("Invalid base directory");
+    }
+
+    protected static String getUnixRelativePath(String base, String path) throws IOException {
+        File f = new File(path);
+        String normalizedPath = FilenameUtils.normalize(f.getAbsolutePath());
+        String rp;
+
+        if(normalizedPath.startsWith(base) && f.exists()) {
+            rp = normalizedPath.substring(base.length());
+        } else {
+            throw new IOException("Invalid file");
+        }
+        rp = FilenameUtils.separatorsToUnix(rp);
+        return rp;
     }
 }
